f138ce168aae458b2906109ff0484bb4512a42b3
Hector Garcia <hg425823@gmail.com>
2024-07-21 21:48:15 -0600

    Update Script Data Base

--- KM.SysControlAdmin.DAL/Document/Script.sql
+++ KM.SysControlAdmin.DAL/Document/Script.sql
@@ -18,6 +18,23 @@ GO
     RegistrationDate DATETIME NOT NULL
     );
 GO
+    CREATE TABLE Trainer(
+    Id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [Name] VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Dui VARCHAR(10) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    Age VARCHAR(3) NOT NULL,
+    Gender VARCHAR(20) NOT NULL,
+    CivilStatus VARCHAR(20) NOT NULL,
+    Phone VARCHAR(9) NOT NULL,
+    [Address] VARCHAR(100) NOT NULL,
+	[Status] TINYINT NOT NULL,
+    CommentsOrObservations VARCHAR(100) NOT NULL,
+    DateCreated DATETIME NOT NULL,
+    DateModification DATETIME NOT NULL,
+    ImageData VARBINARY(MAX) NOT NULL,
+    );
     INSERT INTO [Role] VALUES('Desarrollador');
 GO
     INSERT INTO [User] (IdRole, [Name], LastName, Email, [Password], [Status], RegistrationDate) 
