6d9a084cc1fc4b856314261e19652f4e3a9c8730
Hector Garcia <hg425823@gmail.com>
2024-08-04 23:59:14 -0600

    Creacion de Controller ScheduleController en el proyecto

---
+++ KM.SysControlAdmin.WebApp/Controllers/Schedule - Controller/ScheduleController.cs
@@ -0,0 +1,135 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using KM.SysControlAdmin.BL.Role___BL;
+using KM.SysControlAdmin.BL.Schedule___BL;
+using KM.SysControlAdmin.DAL.Role___DAL;
+using KM.SysControlAdmin.EN.Role___EN;
+using KM.SysControlAdmin.EN.Schedules___EN;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+#endregion
+
+namespace KM.SysControlAdmin.WebApp.Controllers.Schedule___Controller
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Desarrollador")]
+    public class ScheduleController : Controller
+    {
+        // Instancia De La Clase Logica De Negocio
+        ScheduleBL scheduleBL = new ScheduleBL();
+
+        #region METODO PARA INDEX
+        // Metodo Para Mostrar La Vista Index
+        [Authorize(Roles = "Desarrollador")]
+        public async Task<IActionResult> Index(Schedule schedule = null!)
+        {
+            if (schedule == null)
+                schedule = new Schedule();
+
+            var schedules = await scheduleBL.SearchAsync(schedule);
+            return View(schedules);
+        }
+        #endregion
+
+        #region METODO PARA GUARDAR
+        // Metodo Para Mostrar La Vista Guardar
+        [Authorize(Roles = "Desarrollador")]
+        public IActionResult Create()
+        {
+            ViewBag.Error = "";
+            return View();
+        }
+
+        // Metodo Que Recibe y Envia a La Base De Datos
+        [Authorize(Roles = "Desarrollador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Schedule schedule)
+        {
+            try
+            {
+                int result = await scheduleBL.CreateAsync(schedule);
+                TempData["SuccessMessageCreate"] = "Horario Agregado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(schedule);
+            }
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        [Authorize(Roles = "Desarrollador")]
+        // Metodo Para Mostrar La Vista De Modificar
+        public async Task<IActionResult> Edit(int id)
+        {
+            var schedule = await scheduleBL.GetByIdAsync(new Schedule { Id = id });
+            ViewBag.Error = "";
+            return View(schedule);
+        }
+
+        // Metodo Que Recibe y Envia a La Base De Datos
+        [Authorize(Roles = "Desarrollador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Schedule schedule)
+        {
+            try
+            {
+                int result = await scheduleBL.UpdateAsync(schedule);
+                TempData["SuccessMessageUpdate"] = "Horario Modificado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(schedule);
+            }
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Mostrar La Vista De Eliminar
+        [Authorize(Roles = "Desarrollador")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var schedule = await scheduleBL.GetByIdAsync(new Schedule { Id = id });
+            ViewBag.Error = "";
+            return View(schedule);
+        }
+
+        // Metodo Que Recibe y Envia a La Base De Datos
+        [Authorize(Roles = "Desarrollador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Schedule schedule)
+        {
+            try
+            {
+                int result = await scheduleBL.DeleteAsync(schedule);
+                TempData["SuccessMessageDelete"] = "Horario Eliminado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(schedule);
+            }
+        }
+        #endregion
+
+        #region METODO PARA DETALLES
+        // Metodo que Muestra La Vista De Detalles
+        [Authorize(Roles = "Desarrollador")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var schedule = await scheduleBL.GetByIdAsync(new Schedule { Id = id });
+            return View(schedule);
+        }
+        #endregion
+    }
+}
