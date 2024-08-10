93e3a8aa9ee5db6d7e42a67c44301abbf70bff4f
Hector Garcia <hg425823@gmail.com>
2024-08-10 13:24:26 -0600

    Creacion de Clase StudentBL en el proyecto

---
+++ KM.SysControlAdmin.BL/Student - BL/StudentBL.cs
@@ -0,0 +1,65 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using KM.SysControlAdmin.EN.Student___EN;
+using KM.SysControlAdmin.DAL.Student___DAL;
+
+#endregion
+
+namespace KM.SysControlAdmin.BL.Student___BL
+{
+    public class StudentBL
+    {
+        #region METODO PARA CREAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> CreateAsync(Student student)
+        {
+            return await StudentDAL.CreateAsync(student);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> UpdateAsync(Student student)
+        {
+            return await StudentDAL.UpdateAsync(student);
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Eliminar Un Registro Existente En La Base De Datos
+        public async Task<int> DeleteAsync(Student student)
+        {
+            return await StudentDAL.DeleteAsync(student);
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR
+        // Metodo Para Mostrar Una Lista De Registros
+        public async Task<List<Student>> GetAllAsync()
+        {
+            return await StudentDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR POR ID
+        // Metodo Para Mostrar Un Registro Especifico Bajo Un Id
+        public async Task<Student> GetByIdAsync(Student student)
+        {
+            return await StudentDAL.GetByIdAsync(student);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registros Existentes
+        public async Task<List<Student>> SearchAsync(Student student)
+        {
+            return await StudentDAL.SearchAsync(student);
+        }
+        #endregion
+    }
+}
