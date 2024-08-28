fe1a1351599177e2a9c62a84e7c2a0bc44a7f5a8
Hector Garcia <hg425823@gmail.com>
2024-08-28 00:44:38 -0600

    Creacion de la clase CourseAssignmentBL

---
+++ KM.SysControlAdmin.BL/CourseAssignment - BL/CourseAssignmentBL.cs
@@ -0,0 +1,72 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using KM.SysControlAdmin.EN.CourseAssignment___EN;
+using KM.SysControlAdmin.DAL.CourseAssignment___DAL;
+
+#endregion
+
+namespace KM.SysControlAdmin.BL.CourseAssignment___BL
+{
+    public class CourseAssignmentBL
+    {
+        #region METODO PARA CREAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> CreateAsync(CourseAssignment courseAssignment)
+        {
+            return await CourseAssignmentDAL.CreateAsync(courseAssignment);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> UpdateAsync(CourseAssignment courseAssignment)
+        {
+            return await CourseAssignmentDAL.UpdateAsync(courseAssignment);
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Eliminar Un Registro Existente En La Base De Datos
+        public async Task<int> DeleteAsync(CourseAssignment courseAssignment)
+        {
+            return await CourseAssignmentDAL.DeleteAsync(courseAssignment);
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR
+        // Metodo Para Mostrar Una Lista De Registros
+        public async Task<List<CourseAssignment>> GetAllAsync()
+        {
+            return await CourseAssignmentDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR POR ID
+        // Metodo Para Mostrar Un Registro Especifico Bajo Un Id
+        public async Task<CourseAssignment> GetByIdAsync(CourseAssignment courseAssignment)
+        {
+            return await CourseAssignmentDAL.GetByIdAsync(courseAssignment);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registros Existentes
+        public async Task<List<CourseAssignment>> SearchAsync(CourseAssignment courseAssignment)
+        {
+            return await CourseAssignmentDAL.SearchAsync(courseAssignment);
+        }
+        #endregion
+
+        #region METODO PARA INCLUIR PRIVILEGIO Y PRIVILEGIO
+        public async Task<List<CourseAssignment>> SearchIncludeAsync(CourseAssignment courseAssignment)
+        {
+            return await CourseAssignmentDAL.SearchIncludeAsync(courseAssignment);
+        }
+        #endregion
+    }
+}
