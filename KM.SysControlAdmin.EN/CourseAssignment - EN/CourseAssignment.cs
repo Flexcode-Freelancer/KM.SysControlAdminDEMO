8151cf90516a79cd9439ab62374c2b62f94dfd18
Hector Garcia <hg425823@gmail.com>
2024-08-28 00:20:07 -0600

    Creacion de entidad CourseAssignment

---
+++ KM.SysControlAdmin.EN/CourseAssignment - EN/CourseAssignment.cs
@@ -0,0 +1,44 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using KM.SysControlAdmin.EN.Student___EN;
+using KM.SysControlAdmin.EN.Course__EN;
+
+#endregion
+
+namespace KM.SysControlAdmin.EN.CourseAssignment___EN
+{
+    public class CourseAssignment
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Student")]
+        [Required(ErrorMessage = "El Estudiante Es Requerido")]
+        [Display(Name = "Estudiante")]
+        public int IdStudent { get; set; }
+
+        [ForeignKey("Course")]
+        [Required(ErrorMessage = "El Curso Es Requerido")]
+        [Display(Name = "Curso")]
+        public int IdCourse { get; set; }
+
+        [Display(Name = "Fecha de Creacion")]
+        public DateTime DateCreated { get; set; }
+
+        [Display(Name = "Fecha de Modificacion")]
+        public DateTime DateModification { get; set; }
+        #endregion
+
+        public Student? Student { get; set; } // Propiedadd de Navegacion
+
+        public Course? Course { get; set; }// Propiedad de Navegacion
+    }
+}
